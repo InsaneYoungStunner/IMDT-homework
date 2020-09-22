2c453ddaf5561cfdc990ad528380c0398d3d37e7
InsaneYoungStunner <865116882@qq.com>
2020-09-22 11:00:38 +0800

    update

---
+++ Sheep/Assets/Script/Monster.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public GameObject Player;
+    bool zhui = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (zhui)
+        {
+            Vector3 dir = Player.transform.position - transform.position;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 1);
+            transform.Translate(Vector3.forward * Time.deltaTime * 1);//向前走
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            zhui = false;
+        }
+        
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            zhui = true;
+        }
+    }
+}
---
+++ Sheep/Assets/Script/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform groudCheck;
+    public float moveSpeed = 0.1f;
+    public float rotateSpeed =2f;
+    public float groudDis = 0.4f;
+    public LayerMask groudMask;
+    public bool isGround;
+    public UIContrller uIContrller;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        isGround = Physics.CheckSphere(groudCheck.position, groudDis, groudMask);
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (y!=0)
+        {
+            GetComponent<Animator>().SetInteger("animation",2);
+        }
+        else
+        {
+            GetComponent<Animator>().SetInteger("animation", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            GetComponent<Animator>().SetInteger("animation", 3);
+            GetComponent<Rigidbody>().AddForce(new Vector3(0,150,0));
+        }
+
+        transform.Translate(transform.forward*y* moveSpeed, Space.World);
+        transform.Rotate(0,x* rotateSpeed, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "pick")
+        {
+            Destroy(collision.collider.gameObject);
+            uIContrller.n++;
+        }
+    }
+}
