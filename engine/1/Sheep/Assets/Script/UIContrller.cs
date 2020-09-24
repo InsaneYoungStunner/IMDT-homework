3dc378063a9f562ae61e9cbbfa1b845ef12bef51
InsaneYoungStunner <865116882@qq.com>
2020-09-24 20:08:45 +0800

    改变目录结构

---
+++ engine/1/Sheep/Assets/Script/UIContrller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIContrller : MonoBehaviour
+{
+    public int n = 0;
+    public Text score;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        score.text = "得分：" + n;
+    }
+}
