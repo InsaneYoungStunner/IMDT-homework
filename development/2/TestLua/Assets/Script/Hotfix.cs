deca54c0e9487bb3ae7a5787a4a8603e804988a2
InsaneYoungStunner <865116882@qq.com>
2020-09-30 18:06:55 +0800

    提交引擎和开发基础作业2

---
+++ development/2/TestLua/Assets/Script/Hotfix.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using XLua;
+
+namespace XLuaTest
+{
+    [Hotfix]
+    [LuaCallCSharp]
+    public class Hotfix : MonoBehaviour
+    {
+        LuaEnv luaenv = new LuaEnv();
+
+        // Use this for initialization
+        public int hotfixValue = 1;
+        void Start()
+        {
+        
+        }
+
+        public Text showText;
+        void Update()
+        {
+           
+        }
+
+
+
+        public void replacedCode()
+        {
+            print("replacedCode");
+        }
+
+        public void displayUpdate()
+        {
+            replacedCode();
+            print("displayUpdate");
+            if (hotfixValue <= 0.2)
+            {
+                showText.text = "替换成功。hotfixValue已被更新，replacedCode的代码已被替换为更新hotfixValue的代码";
+            }
+            else
+            {
+                showText.text = "hotfixValue为1，没有变化。请单击热更新按钮替换C#方法来更改replacedCode";
+            }
+        }
+
+
+        public void BeginHotFix()
+        {
+
+            print("BeginHotFix");
+            
+            luaenv.DoString(@"
+                xlua.hotfix(CS.XLuaTest.Hotfix, 'replacedCode', function(self) 
+	                    print('hotfixValue Before:',hotfixValue)
+	                    self.hotfixValue=0.1
+	                    print('hotfixValue After:',hotfixValue)
+                end)
+            ");
+        }
+
+       
+        
+    }
+}
---
+++ development/2/TestLua/Assets/XLua/Gen/XLuaTest_HotfixWrap.cs
@@ -0,0 +1,230 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class XLuaTestHotfixWrap 
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			System.Type type = typeof(XLuaTest.Hotfix);
+			Utils.BeginObjectRegister(type, L, translator, 0, 3, 2, 2);
+			
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "replacedCode", _m_replacedCode);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "displayUpdate", _m_displayUpdate);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "BeginHotFix", _m_BeginHotFix);
+			
+			
+			Utils.RegisterFunc(L, Utils.GETTER_IDX, "hotfixValue", _g_get_hotfixValue);
+            Utils.RegisterFunc(L, Utils.GETTER_IDX, "showText", _g_get_showText);
+            
+			Utils.RegisterFunc(L, Utils.SETTER_IDX, "hotfixValue", _s_set_hotfixValue);
+            Utils.RegisterFunc(L, Utils.SETTER_IDX, "showText", _s_set_showText);
+            
+			
+			Utils.EndObjectRegister(type, L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(type, L, __CreateInstance, 1, 0, 0);
+			
+			
+            
+			
+			
+			
+			Utils.EndClassRegister(type, L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            
+			try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+				if(LuaAPI.lua_gettop(L) == 1)
+				{
+					
+					var gen_ret = new XLuaTest.Hotfix();
+					translator.Push(L, gen_ret);
+                    
+					return 1;
+				}
+				
+			}
+			catch(System.Exception gen_e) {
+				return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+			}
+            return LuaAPI.luaL_error(L, "invalid arguments to XLuaTest.Hotfix constructor!");
+            
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_replacedCode(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                XLuaTest.Hotfix gen_to_be_invoked = (XLuaTest.Hotfix)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    
+                    gen_to_be_invoked.replacedCode(  );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_displayUpdate(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                XLuaTest.Hotfix gen_to_be_invoked = (XLuaTest.Hotfix)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    
+                    gen_to_be_invoked.displayUpdate(  );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_BeginHotFix(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                XLuaTest.Hotfix gen_to_be_invoked = (XLuaTest.Hotfix)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    
+                    gen_to_be_invoked.BeginHotFix(  );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_hotfixValue(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                XLuaTest.Hotfix gen_to_be_invoked = (XLuaTest.Hotfix)translator.FastGetCSObj(L, 1);
+                LuaAPI.xlua_pushinteger(L, gen_to_be_invoked.hotfixValue);
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 1;
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_showText(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                XLuaTest.Hotfix gen_to_be_invoked = (XLuaTest.Hotfix)translator.FastGetCSObj(L, 1);
+                translator.Push(L, gen_to_be_invoked.showText);
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 1;
+        }
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _s_set_hotfixValue(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                XLuaTest.Hotfix gen_to_be_invoked = (XLuaTest.Hotfix)translator.FastGetCSObj(L, 1);
+                gen_to_be_invoked.hotfixValue = LuaAPI.xlua_tointeger(L, 2);
+            
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 0;
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _s_set_showText(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                XLuaTest.Hotfix gen_to_be_invoked = (XLuaTest.Hotfix)translator.FastGetCSObj(L, 1);
+                gen_to_be_invoked.showText = (UnityEngine.UI.Text)translator.GetObject(L, 2, typeof(UnityEngine.UI.Text));
+            
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 0;
+        }
+        
+		
+		
+		
+		
+    }
+}
