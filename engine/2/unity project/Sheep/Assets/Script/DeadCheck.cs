b2984a66e797639268b090f9869d30c118268963
InsaneYoungStunner <865116882@qq.com>
2020-11-29 17:08:26 +0800

    作业提交

---
+++ engine/2/unity project/Sheep/Assets/Script/DeadCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DeadCheck : MonoBehaviour
+{
+
+    public GameObject deadPage;
+    public Text score;
+    public UIContrller uIContrller;
+    void Start()
+    {
+        uIContrller = FindObjectOfType<UIContrller>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            collision.collider.GetComponent<PlayerController>().enabled = false;
+            collision.collider.GetComponent<Animator>().SetInteger("animation",8);
+            deadPage.SetActive(true);
+            score.text = "得分：" + uIContrller.n;
+        }
+    }
+}
